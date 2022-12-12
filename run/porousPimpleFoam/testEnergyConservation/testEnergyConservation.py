cda3a5a0194d411aac5c380297a11a8fe858ece5
Jakob Hærvig <jakob.haervig@gmail.com>
2022-12-12 15:14:16 +0100

    Update

--- run/porousPimpleFoam/testEnergyConservation/testEnergyConservation.py
+++ run/porousPimpleFoam/testEnergyConservation/testEnergyConservation.py
@@ -4,12 +4,16 @@ import matplotlib.pyplot as plt
 time, Tf, Ts = np.loadtxt('postProcessing/volFieldValue1/0/volFieldValue.dat', unpack=True)
 
 rhof = 1.2
-rhos = 3000
+rhos = 5150
 
 cpf = 700
-cps = 0.1
+cps = 1130
+
+V = 1
+
+porosity = 0.5
 
 print("Total enthalpy:", Tf*rhof*cpf + Ts*rhos*cps)
 
-plt.plot(time,Tf*rhof*cpf, time,Ts*rhos*cps, time, (Ts*rhos*cps+Tf*rhof*cpf)/2)
+plt.plot(time,Tf*rhof*cpf*porosity*V, time,Ts*rhos*cps*(1-porosity)*V, time, (Ts*rhos*cps*porosity*V+Tf*rhof*cp*(1-porosity)*V)/2)
 plt.savefig('foo.pdf')
